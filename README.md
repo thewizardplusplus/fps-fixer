acda4ae5028647b116910e783cbf429f05e5a475
thewizardplusplus <thewizardplusplus@yandex.ru>
2023-09-05 17:25:04 +0600

    Iterate over videos

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ $ fps_fixer.bash
 
 Environment variables:
 
+- `VIDEO_EXTENSION` &mdash; video file extension (default: `mp4`);
 - `FIXED_VIDEO_BASE_PATH` &mdash; base path for fixed videos (default: `./fixed-videos`).
 
 ## License
--- fps_fixer.bash
+++ fps_fixer.bash
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+declare -r VIDEO_EXTENSION="${VIDEO_EXTENSION-mp4}"
 declare -r FIXED_VIDEO_BASE_PATH="${FIXED_VIDEO_BASE_PATH-./fixed-videos}"
 
 mkdir --parents "$FIXED_VIDEO_BASE_PATH"
+
+find -maxdepth 1 -name "*.$VIDEO_EXTENSION" \
+  | while read -r; do
+    declare video_path="$REPLY"
+    echo "process video $video_path"
+  done
